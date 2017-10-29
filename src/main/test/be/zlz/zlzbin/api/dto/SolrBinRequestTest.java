285cf7c6144a663e47bfdb98953a3247b236a158
Frederik Van Herbruggen <Frederik.vanh@gmail.com>
2017-10-29 21:25:37 +0100

    added rudimentary solr support

---
+++ src/main/test/be/zlz/zlzbin/api/dto/SolrBinRequestTest.java
@@ -0,0 +1,34 @@
+package be.zlz.zlzbin.api.dto;
+
+import be.zlz.zlzbin.api.domain.Bin;
+import be.zlz.zlzbin.api.domain.Request;
+import be.zlz.zlzbin.api.services.SolrService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SolrBinRequestTest {
+
+    @Test
+    public void run(){
+        Request request = new Request(HttpMethod.GET, "body", "http1", new HashMap<>());
+        Bin b = new Bin();
+        b.setName("bin");
+        request.setBin(b);
+
+        SolrService solrService = new SolrService();
+        solrService.add(request);
+    }
+
+    @Test
+    public void query(){
+        SolrService solrService = new SolrService();
+        List<Integer> ids = solrService.query("*:*");
+        System.out.println(ids);
+    }
+}
