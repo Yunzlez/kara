f2d3ea300c57a68da643830d4f228640f400dcc2
Frederik Van Herbruggen <frederik.vanh+accounts@gmail.com>
2019-05-13 18:16:55 +0200

    Db stuff

---
+++ bin/src/main/resources/db/migration/V4__innodb.sql
@@ -0,0 +1,12 @@
+USE kara;
+ALTER TABLE bin ENGINE=InnoDB;
+ALTER TABLE binary_request ENGINE=InnoDB;
+ALTER TABLE bin_config ENGINE=InnoDB;
+ALTER TABLE reply ENGINE=InnoDB;
+ALTER TABLE reply_cookies ENGINE=InnoDB;
+ALTER TABLE reply_headers ENGINE=InnoDB;
+ALTER TABLE request ENGINE=InnoDB;
+ALTER TABLE request_headers ENGINE=InnoDB;
+ALTER TABLE request_metric ENGINE=InnoDB;
+ALTER TABLE request_metric_counts ENGINE=InnoDB;
+ALTER TABLE request_query_params ENGINE=InnoDB;
